d5561ef4461bd7daf022baaa0bd3b9e3f06fe847
aryangupta8970 <aryan.professional09@gmail.com>
2024-10-20 08:22:36 +0530

    Add super hard pattern printing solutions

---
+++ Pattern Printing/Super Hard/question08.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    for (row = 1; row <= 5; row = row + 1)
+    {
+        for (col = 1; col <= 5 - row; col++)
+        {
+            cout << "  ";
+        }
+
+        // increasing order
+        for (col = 1; col <= row; col++)
+        {
+            char name = 'A' + (col - 1);
+            cout << name << " ";
+        }
+
+        // decreasing order
+        for (col = row - 1; col >= 1; col = col - 1)
+        {
+            char name = 'A' + (col - 1);
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
